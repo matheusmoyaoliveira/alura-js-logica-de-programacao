a775f89c25699e5525250534b303e47b6ad44131
Matheus Moya de Oliveira <167132031+matheusmoyaoliveira@users.noreply.github.com>
2024-09-30 20:34:48 -0300

    Primeiro desafio

---
+++ projetos/logica-js-projeto-inicial/desafio-alura/desafio.js
@@ -0,0 +1,12 @@
+alert('Boas vindas ao nosso site!');
+let nome = prompt('Qual o seu nome? ');
+let idade = prompt('Qual sua idade? ');
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!');
+}
+
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
